ed8ed03de4de573933e8481ca50d7bbb1d45f1aa
yewwoei <yewwoei.low@outlook.com>
2018-12-16 22:15:48 +0800

    started day 3

---
+++ 3/day3.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def part1(input_data: List[str]):
+    return
+
+
+if __name__ == "__main__":
+    part1_input = open("input1.txt", 'r')
+    print(f"The answer to part 1 is: {part1(part1_input.splitlines())}")
